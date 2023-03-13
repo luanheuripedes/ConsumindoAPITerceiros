0459a9124fb895d24696cb339dbdfab7ae9cc917
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2023-03-13 11:48:45 -0300

    implementado o serviço de enderecoService

--- Interfaces/IEnderecoService.cs
+++ Interfaces/IEnderecoService.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IntegraBrasilApi.DTOs;
 
 namespace IntegraBrasilApi.Interfaces
 {
     public interface IEnderecoService
     {
-        
+        Task<ResponseGenericoDTO<EnderecoResponseDTO>> BuscarEndereco(string cep);
     }
 }
--- Services/EnderecoService.cs
+++ Services/EnderecoService.cs
@@ -2,11 +2,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using IntegraBrasilApi.DTOs;
+using IntegraBrasilApi.Interfaces;
 
 namespace IntegraBrasilApi.Services
 {
-    public class EnderecoService
+    public class EnderecoService : IEnderecoService
     {
-        
+        private readonly IBrasilAPi _brasilAPI;
+        private readonly IMapper _mapper;
+
+        public EnderecoService(IBrasilAPi brasilAPI, IMapper mapper)
+        {
+            _brasilAPI = brasilAPI;
+            _mapper = mapper;
+        }
+
+        public async Task<ResponseGenericoDTO<EnderecoResponseDTO>> BuscarEndereco(string cep)
+        {
+            var endereco = await _brasilAPI.BuscarEnderecoPorCEP(cep);
+            return _mapper.Map<ResponseGenericoDTO<EnderecoResponseDTO>>(endereco);
+        }
     }
 }
