bf0539bf2d59a0963bd28cec2dc7c2e433b5c2e0
luanheuripedes <luanheuripedesmoreiradepaula@gmail.com>
2023-03-13 11:35:53 -0300

    Implementado interface IBrasilAPi e implementado Rest(consumir a API terceira de fato)

--- Interfaces/IBrasilAPi.cs
+++ Interfaces/IBrasilAPi.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IntegraBrasilApi.DTOs;
+using IntegraBrasilApi.Models;
 
 namespace IntegraBrasilApi.Interfaces
 {
     public interface IBrasilAPi
     {
-        
+        Task<ResponseGenericoDTO<EnderecoModel>> BuscarEnderecoPorCEP(string cep);
+        Task<ResponseGenericoDTO<List<BancoModel>>> BuscarTodosBancos();
+        Task<ResponseGenericoDTO<BancoModel>> BuscarBanco(string codigoBanco);
     }
 }
--- Rest/BrasilAPIRest.cs
+++ Rest/BrasilAPIRest.cs
@@ -1,12 +1,47 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Dynamic;
+using System.Text.Json;
+using IntegraBrasilApi.DTOs;
+using IntegraBrasilApi.Interfaces;
+using IntegraBrasilApi.Models;
 
 namespace IntegraBrasilApi.Rest
 {
-    public class BrasilAPIRest
+    public class BrasilAPIRest : IBrasilAPi
     {
+        public async Task<ResponseGenericoDTO<EnderecoModel>> BuscarEnderecoPorCEP(string cep)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, $"https://brasilapi.com.br/api/cep/v1/{cep}");
+
+            var response = new ResponseGenericoDTO<EnderecoModel>();
+
+            using (var client = new HttpClient()){
+                var responseBrasilAPI = await client.SendAsync(request);
+                var conteudoResponse = await responseBrasilAPI.Content.ReadAsStringAsync();
+                var objResponse = JsonSerializer.Deserialize<EnderecoModel>(conteudoResponse);
+
+
+                if(responseBrasilAPI.IsSuccessStatusCode){
+                    response.CodigoHttp = responseBrasilAPI.StatusCode;
+                    response.DadosRetorno = objResponse;
+                }else{
+                    response.CodigoHttp = responseBrasilAPI.StatusCode;
+                    response.ErroRetorno = JsonSerializer.Deserialize<ExpandoObject>(conteudoResponse);
+                }
+            }
+
+            return response;
+        }
+
+        public async Task<ResponseGenericoDTO<List<BancoModel>>> BuscarTodosBancos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ResponseGenericoDTO<BancoModel>> BuscarBanco(string codigoBanco)
+        {
+            throw new NotImplementedException();
+        }
+
         
     }
 }
